8bab52c9ae03973b80accfb21279d55a9fc54972
Nageswara Rao <nrao@INDMP0018.local>
2022-03-13 20:26:56 +0530

    React-table-first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,261 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { useTable, useFilters, useGlobalFilter,useAsyncDebounce,useRowSelect} from 'react-table'
+// A great library for fuzzy filtering/sorting items
+import {matchSorter} from 'match-sorter'
+import axios from 'axios';
+import Button from 'react-bootstrap/Button'
+import {FaTrashIt} from "react-icons/fa";
+import {FaUserAlt} from "react-icons/fa";
+import {FaEdit} from "react-icons/fa"
+import { Checkbox } from './Checkbox';
 
-function App() {
+// import makeData from './makeData'
+
+
+
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+
+
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search3 ${count} records...`}
+    />
+  )
 }
 
-export default App;
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+// Our table component
+function Table({ columns, data }) {
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, // Be sure to pass the defaultColumn option
+      filterTypes,
+    },
+    
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        {
+          id: 'selection',
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <Checkbox {...getToggleAllRowsSelectedProps()} />
+          ),
+          Cell: ({ row }) => <Checkbox {...row.getToggleRowSelectedProps()} />
+        },
+        ...columns
+      ])
+    },
+    useFilters, // useFilters!
+    useGlobalFilter, // useGlobalFilter!
+    useRowSelect
+  )
+
+  // We don't want to render all of the rows for this example, so cap
+  // it for this use case
+  const firstPageRows = rows.slice(0, 20)
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                  {/* Render the columns filter UI */}
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              ))}
+            </tr>
+          ))}
+          
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+      <div>
+        <pre>
+          <code>{JSON.stringify(state.filters, null, 2)}</code>
+        </pre>
+      </div>
+    </>
+  )
+}
+
+
+
+function App() {
+
+  const [data, setData] = useState([])
+
+  function fetchdata() {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(res => {setData(res.data)})
+  }
+
+  useEffect(()=>fetchdata(),[])
+  
+
+  const columns = React.useMemo(
+    () => [
+      // {
+      //   Header: 'Select',
+      //   accessor: 'checkbox', // accessor is the "key" in the data
+      //   disableFilters:true,
+      //   Cell: row => {
+      //     return(
+      //       <div style={{'textAlign':'center'}}>
+      //         <input type="checkbox" 
+      //           defaultChecked={row.value === "Yes" ? true : false} onClick={()=>console.log(row)}  />
+      //       </div>)}
+      // },
+      {
+        Header: 'First Name',
+        accessor: 'name' // accessor is the "key" in the data
+      },
+      {
+        Header: 'Last Name',
+            accessor: 'username',
+            // Use our custom `fuzzyText` filter on this column
+            filter: 'fuzzyText',
+      },
+      {
+      Header: 'Email',
+      accessor: 'email',
+            // Use our custom `fuzzyText` filter on this column
+            
+      },
+
+      {
+        Header: 'Action',
+        accessor: 'actions',// Use our custom `fuzzyText` filter on this column
+        disableFilters:true,
+        Cell: ({row}) =>{
+          return(
+            <div>
+              <Button onClick={()=>console.log(row)}>{FaUserAlt}</Button>
+              <Button onClick={()=>console.log(row.id)}>{FaEdit}</Button>
+            </div>
+          )
+        }
+
+        
+      
+              
+        },
+      
+    ],
+    []
+)
+  
+
+  
+  const udata = React.useMemo(() => data, [data])
+  
+
+  return (
+    
+    <Styles>
+      <Table columns={columns} data={udata} />
+    </Styles>
+    
+  )
+}
+
+export default App
---
+++ src/waste.js
@@ -0,0 +1,197 @@
+
+// Define a default UI for filtering
+// function GlobalFilter({
+//   preGlobalFilteredRows,
+//   globalFilter,
+//   setGlobalFilter,
+// }) {
+//   const count = preGlobalFilteredRows.length
+//   const [value, setValue] = React.useState(globalFilter)
+//   const onChange = useAsyncDebounce(value => {
+//     setGlobalFilter(value || undefined)
+//   }, 200)
+
+//   return (
+//     <span>
+//       Search3:{' '}
+//       <input
+//         value={value || ""}
+//         onChange={e => {
+//           setValue(e.target.value);
+//           onChange(e.target.value);
+//         }}
+//         placeholder={`${count} records...`}
+//         style={{
+//           fontSize: '1.1rem',
+//           border: '0',
+//         }}
+//       />
+//     </span>
+//   )
+// }
+
+// Define a default UI for filtering
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+// function SelectColumnFilter({
+//   column: { filterValue, setFilter, preFilteredRows, id },
+// }) {
+//   // Calculate the options for filtering
+//   // using the preFilteredRows
+//   const options = React.useMemo(() => {
+//     const options = new Set()
+//     preFilteredRows.forEach(row => {
+//       options.add(row.values[id])
+//     })
+//     return [...options.values()]
+//   }, [id, preFilteredRows])
+
+//   // Render a multi-select box
+//   return (
+//     <select
+//       value={filterValue}
+//       onChange={e => {
+//         setFilter(e.target.value || undefined)
+//       }}
+//     >
+//       <option value="">All</option>
+//       {options.map((option, i) => (
+//         <option key={i} value={option}>
+//           {option}
+//         </option>
+//       ))}
+//     </select>
+//   )
+// }
+
+// This is a custom filter UI that uses a
+// slider to set the filter value between a column's
+// min and max values
+// function SliderColumnFilter({
+//   column: { filterValue, setFilter, preFilteredRows, id },
+// }) {
+//   // Calculate the min and max
+//   // using the preFilteredRows
+
+//   const [min, max] = React.useMemo(() => {
+//     let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+//     let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+//     preFilteredRows.forEach(row => {
+//       min = Math.min(row.values[id], min)
+//       max = Math.max(row.values[id], max)
+//     })
+//     return [min, max]
+//   }, [id, preFilteredRows])
+
+//   return (
+//     <>
+//       <input
+//         type="range"
+//         min={min}
+//         max={max}
+//         value={filterValue || min}
+//         onChange={e => {
+//           setFilter(parseInt(e.target.value, 10))
+//         }}
+//       />
+//       <button onClick={() => setFilter(undefined)}>Off</button>
+//     </>
+//   )
+// }
+
+// This is a custom UI for our 'between' or number range
+// filter. It uses two number boxes and filters rows to
+// ones that have values between the two
+// function NumberRangeColumnFilter({
+//   column: { filterValue = [], preFilteredRows, setFilter, id },
+// }) {
+//   const [min, max] = React.useMemo(() => {
+//     let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+//     let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+//     preFilteredRows.forEach(row => {
+//       min = Math.min(row.values[id], min)
+//       max = Math.max(row.values[id], max)
+//     })
+//     return [min, max]
+//   }, [id, preFilteredRows])
+
+//   return (
+//     <div
+//       style={{
+//         display: 'flex',
+//       }}
+//     >
+//       <input
+//         value={filterValue[0] || ''}
+//         type="number"
+//         onChange={e => {
+//           const val = e.target.value
+//           setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
+//         }}
+//         placeholder={`Min (${min})`}
+//         style={{
+//           width: '70px',
+//           marginRight: '0.5rem',
+//         }}
+//       />
+//       to
+//       <input
+//         value={filterValue[1] || ''}
+//         type="number"
+//         onChange={e => {
+//           const val = e.target.value
+//           setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
+//         }}
+//         placeholder={`Max (${max})`}
+//         style={{
+//           width: '70px',
+//           marginLeft: '0.5rem',
+//         }}
+//       />
+//     </div>
+//   )
+// }
+
+// Define a custom filter filter function!
+// function filterGreaterThan(rows, id, filterValue) {
+//   return rows.filter(row => {
+//     const rowValue = row.values[id]
+//     return rowValue >= filterValue
+//   })
+// }
+
+// This is an autoRemove method on the filter function that
+// when given the new filter value and returns true, the filter
+// will be automatically removed. Normally this is just an undefined
+// check, but here, we want to remove the filter if it's not a number
+// filterGreaterThan.autoRemove = val => typeof val !== 'number'
+
+
+<tr>
+            {/* <th
+              colSpan={visibleColumns.length}
+              style={{
+                textAlign: 'left',
+              }}
+            >
+              <GlobalFilter
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+              />
+            </th> */}
+          </tr>
+
+
+// {
+      //   Header: 'Select',
+      //   accessor: 'checkbox', // accessor is the "key" in the data
+      //   disableFilters:true,
+      //   Cell: row => {
+      //     return(
+      //       <div style={{'textAlign':'center'}}>
+      //         <input type="checkbox" 
+      //           defaultChecked={row.value === "Yes" ? true : false} onClick={()=>console.log(row)}  />
+      //       </div>)}
+      // },
